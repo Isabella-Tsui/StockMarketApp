eb7db19d860512bfed9f2f029183e5e405408cc5
Isabella Tsui <126940962+Isabella-Tsui@users.noreply.github.com>
2023-07-15 18:40:28 -0700

    Added logout endpoint

--- back-end/server.js
+++ back-end/server.js
@@ -6,22 +6,6 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const mysql = require("mysql");
 
-/*          ---STEP 0 from the README file---
-    Enter your specific database information on lines 23-26.
-    This information will be found on your AWS account
-    As an example:
-    host: 'xxxxxxxx.us-west-2.rds.amazonaws.com',
-    user: 'billy_the_programmer',
-    password: 'programming_rules',
-    database: 'cool_database_name'
-*/
-// const connection = mysql.createConnection({
-//     host: 'xxxxxxxx.us-west-2.rds.amazonaws.com',
-//     user: '<your_username>',
-//     password: '<your_password>',
-//     database: '<your_database_name>'
-// })
-
 const connection = mysql.createConnection({
   host: "stockmarketapp.cqiv4llmmhyd.ca-central-1.rds.amazonaws.com",
   user: "itsui",
@@ -51,7 +35,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 connection.connect();
 
-//Added
+//Added //LOOK AT THIS
 app.use(
   session({
     key: "asdf",
@@ -62,40 +46,11 @@ app.use(
     cookie: {
       maxAge: 1825 * 86400 * 1000,
       httpOnly: false,
+      sameSite: "None",
+      secure: true,
     },
   })
 );
-
-/*
-    Here is an example get request to an endpoint called members.
-    You can use it as a starting point for your own database.
-    That being said, you will likely need to change the query 
-    unless your database also contains a table called users with 
-    the a column called first_name.
-
-    Once you have added your database credentials on lines 16-19
-    you can uncomment lines 47-57 alter the query
-    to work with your database
-
-    If you want the corresponding front-end to work, I recommend
-    a query that returns one column of data.
-    Ex: SELECT <column_name> FROM <table_name>
-*/
-
-// app.get("/members", (req, res) => {
-//   connection.query(
-//     "SELECT first_name FROM users ORDER BY first_name ASC",
-//     (err, rows) => {
-//       if (err) throw err;
-//       let response = [];
-//       for (let row of rows) {
-//         response.push({ first_name: row.first_name });
-//       }
-//       // This reponse (i.e. data returned from the query) gets sent back to the front-end
-//       res.send(response);
-//     }
-//   );
-// });
 
 app.post("/login", async (req, res) => {
   let username = req.body.username;
@@ -225,6 +180,7 @@ app.post("/register", async (req, res) => {
   );
 });
 
+//Added
 app.post("/logout", (req, res) => {
   if (req.session.userId) {
     req.session.destroy();
@@ -232,34 +188,6 @@ app.post("/logout", (req, res) => {
       success: true,
     });
     return true;
-  } else {
-    res.json({
-      success: false,
-    });
-    return false;
-  }
-});
-
-app.post("/isLoggedIn", (req, res) => {
-  if (req.session.userId) {
-    let currUser = [req.session.userId];
-    connection.query(
-      "SELECT * FROM users WHERE id=? LIMIT 1",
-      currUser,
-      (err, data, fields) => {
-        if (data && data.length === 1) {
-          res.json({
-            success: true,
-            username: data[0].username,
-          });
-          return true;
-        } else {
-          res.json({
-            success: true,
-          });
-        }
-      }
-    );
   } else {
     res.json({
       success: false,
